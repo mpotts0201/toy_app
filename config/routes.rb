8900284f7eb3832a6aee200a203c542280a2decd
mpotts0201 <mpotts0201@gmail.com>
2018-03-26 13:49:13 -0400

    User index now iterating and rendering users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'application#hello'
-  
+
+  get '/users', to: 'users#index'
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
